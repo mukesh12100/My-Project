549447f835e3ef2e53aa711e740231a50105c614
mukesh12100 <mukeshpawar12100@gmail.com>
2022-11-07 19:20:43 +0000

    Project.Java has been committed

---
+++ project.java
@@ -0,0 +1,27 @@
+[200~<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome to B&B Store</title>
+</head>
+<body>
+<h1>Welcome to B&B Stores</h1>
+
+	<div class="Search">
+
+	<input type="Explore More" name="Explore More"
+
+	placeholder="Explore More" required="required"/>
+
+	<button type="Search" class="btn btn-primary btn-block btn-large">search</button><br>
+
+	</div>
+		
+		<input type="submit" value="Clothing">
+			<input type="submit" value="Footwear">
+				<input type="submit" value="Accessories">
+					<input type="submit" value="And Many More!">
+						
+						
+					</body>
+					</html>
